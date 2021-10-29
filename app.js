0fab1494716a3ebc331e647bd6ed9d1ddd07aaf0
unknown <priyankaghansela22@gmail.com>
2021-10-29 13:11:34 +0530

    minor changes

--- app.js
+++ app.js
@@ -47,3 +47,6 @@ app.listen(port,()=>{
 })
 
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
